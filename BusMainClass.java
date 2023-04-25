334eb44eade9b29dec260d623a9c8bd89fe0f61e
Dineshkumar S <91516384+dineshmechanical@users.noreply.github.com>
2023-04-26 00:21:38 +0530

    Update BusMainClass.java

--- BusMainClass.java
+++ BusMainClass.java
@@ -1,38 +1,35 @@
-package busResv;
-
+package busReser;
 import java.util.Scanner;
 import java.util.ArrayList;
-
 public class BusDemo {
-
 	public static void main(String[] args) {
-		
-	
-		ArrayList<Bus> buses = new ArrayList<Bus>();
+		ArrayList<Bus> busses = new ArrayList<Bus>();
 		ArrayList<Booking> bookings = new ArrayList<Booking>();
 		
-		buses.add(new Bus(1,true,2));
-		buses.add(new Bus(2,false,50));
-		buses.add(new Bus(3,true,48));
+		busses.add(new Bus(1,true,2));
+		busses.add(new Bus(2,false,50));
+		busses.add(new Bus(3,true,48));
 		
-		int userOpt = 1;
+		int UserOpt=1;
 		Scanner scanner = new Scanner(System.in);
 		
-		for(Bus b:buses) {
+		for(Bus b:busses) {
 			b.displayBusInfo();
 		}
 		
-		while(userOpt==1) {
-			System.out.println("Enter 1 to Book and 2 to exit");
-			userOpt = scanner.nextInt();
-			if(userOpt == 1) {
+		while(UserOpt==1) {		
+			System.out.println("Enter 1 to Book and 2 to Exit ");
+			UserOpt = scanner.nextInt(); 
+			if(UserOpt==1) {
 				Booking booking = new Booking();
-				if(booking.isAvailable(bookings,buses)) {
+				if(booking.isAvailable(bookings,busses)) {
 					bookings.add(booking);
-					System.out.println("Your booking is confirmed");
+					System.out.println("Your booking is confirmed ");
+					
 				}
-				else
-					System.out.println("Sorry. Bus is full. Try another bus or date.");
+				else {
+					System.out.println("Sorry Bus is full ");
+				}
 			}
 		}
 	}
