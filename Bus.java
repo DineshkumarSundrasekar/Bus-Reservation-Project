75cb2fba354418ff79eb4cf3d789360999a280c3
Dineshkumar S <91516384+dineshmechanical@users.noreply.github.com>
2023-04-26 00:23:18 +0530

    Update Bus.java

--- Bus.java
+++ Bus.java
@@ -1,36 +1,32 @@
-package busResv;
+package busReser;
 
 public class Bus {
 	private int busNo;
 	private boolean ac;
-	private int capacity; 
+	private int capacity;
 	
 	Bus(int no,boolean ac,int cap){
 		this.busNo = no;
 		this.ac = ac;
 		this.capacity = cap;
 	}
-	
-	public int getBusNo(){ 
+	public int getBusNo() {
 		return busNo;
 	}
-	
-	public boolean isAc(){
+	public boolean isAc() {
 		return ac;
 	}
 	public int getCapacity(){
 		return capacity;
 	}
 	
-	public void setAc(boolean val) {
-		ac = val;
+	public void setAc(boolean ac) {
+		ac = ac;
 	}
-	
 	public void setCapacity(int cap) {
 		capacity = cap;
 	}
-	
 	public void displayBusInfo(){
-		System.out.println("Bus No:" + busNo + " Ac:" + ac + " Total Capacity: " + capacity);
+		System.out.println("BusNo: "+ busNo + " Ac: " + ac + " Total Seat: " + capacity);
 	}
 }
